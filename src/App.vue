55d009f456008398050ae3b4f45b3c0f2df093f5
744461195@qq.com <744461195@qq.com>
2019-07-24 15:19:17 +0800

    new tabbar

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <MainTabBar/>
+  </div>
+</template>
+
+<script>
+import MainTabBar from './components/MainTabBat'
+export default {
+  name: 'app',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+  
+ 
+
+</style>
